eeede45dc466a8e45997e125b4676d45d7cbcc9e
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-01-29 00:40:18 +0300

    create the app logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,14 @@
-import React from "react";
-
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <header className="flex justify-between px-3 py-2">
+      <a
+        href="#!"
+        className="text-4xl font-medium border-2 border-green-400 px-2 pb-1 rounded-md"
+      >
+        R<span className="font-extrabold text-green-400">O</span>
+        <span className="font-extrabold text-orange-400">_</span>
+        <span className="font-extrabold text-green-400">O</span>M
+      </a>
+    </header>
+  );
 }
