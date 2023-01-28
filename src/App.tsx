b44f2aab77c440f38693fbe242e7b74f6861d8d8
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-01-29 00:47:56 +0300

    create the index route and render the Home page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+
 import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
