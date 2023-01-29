c48b5a5eaf02c3c94d26c65a7f0b2a5f16db72e8
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-01-29 12:04:25 +0300

    import and wrap the routes with the Layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import NavBar from "./components/NavBar";
+import Layout from "./layout/Layout";
 import NotFoundPage from "./pages/404";
 import Home from "./pages/Home";
 
@@ -8,10 +9,12 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/*" element={<NotFoundPage />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/*" element={<NotFoundPage />} />
+        </Routes>
+      </Layout>
     </div>
   );
 }
