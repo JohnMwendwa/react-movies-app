a6806a06bba205b671b382c5d3af42cf710df452
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-01-29 13:14:21 +0300

    define context props

--- src/contexts/searchMoviesContext.tsx
+++ src/contexts/searchMoviesContext.tsx
@@ -6,11 +6,57 @@ import {
   useContext,
 } from "react";
 
+interface SearchMoviesContextProps {
+  movies: Movie[];
+  query: string;
+  setQuery: () => void;
+}
+
+interface Movie {
+  id: number;
+  url: string;
+  imd_code: string;
+  title: string;
+  title_english: string;
+  title_long: string;
+  slug: string;
+  year: number;
+  rating: number;
+  runtime: number;
+  genres: string[];
+  summary: string;
+  description_full: string;
+  synopsis: string;
+  yt_trailer_code: string;
+  language: string;
+  mpa_rating: string;
+  background_image: string;
+  background_image_original: string;
+  small_cover_image: string;
+  medium_cover_image: string;
+  large_cover_image: string;
+  state: string;
+  torrents: [
+    {
+      url: string;
+      hash: string;
+      quality: string;
+      type: string;
+      seed: number;
+      peers: number;
+      size: string;
+      size_bytes: number;
+      date_uploaded: string;
+      date_uploaded_unix: number;
+    }
+  ];
+}
+
 interface SearchMoviesProviderProps {
   children: ReactNode;
 }
 
-const SearchMoviesContext = createContext({});
+const SearchMoviesContext = createContext({} as SearchMoviesContextProps);
 
 export const useSearchMoviesContext = () => {
   return useContext(SearchMoviesContext);
