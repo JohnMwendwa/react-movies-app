861a144eeef4be2a2230741fa30ec18d0679489b
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-01-29 20:02:35 +0300

    add same heading as the official yts movie site

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1 className="font-bold text-3xl text-center my-2 hidden md:block">
+        Download YTS YIFY movies: HD smallest size
+      </h1>
+    </div>
+  );
 }
