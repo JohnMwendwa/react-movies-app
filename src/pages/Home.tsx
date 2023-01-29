2980ffadfddd6fb6ec139a74a5cb9e40a3eac411
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-01-29 20:10:19 +0300

    map through the movies array and render indvidual movie cards

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import MovieCard from "../components/MovieCard";
+import { useSearchMoviesContext } from "../contexts/searchMoviesContext";
 
 export default function Home() {
+  const { movies } = useSearchMoviesContext();
   return (
     <div>
       <h1 className="font-bold text-3xl text-center my-2 hidden md:block">
         Download YTS YIFY movies: HD smallest size
       </h1>
+      <div>
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} {...movie} />
+        ))}
+      </div>
     </div>
   );
 }
