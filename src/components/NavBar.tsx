584434c786ac4a4b5d05b34b56526c2765c7750c
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-01-29 12:17:10 +0300

    set max-width for the Navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,16 @@
 export default function NavBar() {
   return (
-    <header className="flex justify-between px-3 py-2">
-      <a
-        href="#!"
-        className="text-4xl font-medium border-2 border-green-400 px-2 pb-1 rounded-md"
-      >
-        R<span className="font-extrabold text-green-400">O</span>
-        <span className="font-extrabold text-orange-400">_</span>
-        <span className="font-extrabold text-green-400">O</span>M
-      </a>
+    <header className="border-b ">
+      <div className="flex justify-between px-3 py-2">
+        <a
+          href="/"
+          className="text-4xl font-medium border-2 border-green-400 px-2 pb-1 rounded-md"
+        >
+          R<span className="font-extrabold text-green-400">O</span>
+          <span className="font-extrabold text-orange-400">_</span>
+          <span className="font-extrabold text-green-400">O</span>M
+        </a>
+      </div>
     </header>
   );
 }
