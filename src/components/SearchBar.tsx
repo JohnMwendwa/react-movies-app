50ddb42f532d22621af98f5aaf6cedce64703afb
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-01-30 21:07:32 +0300

    style the SearchBar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -8,7 +8,8 @@ function SearchBar() {
         type="text"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        placeholder="Quick Search movvies..."
+        placeholder="Quick Search Movies..."
+        className="border border-green-400 outline-green-400 px-4 py-1 rounded-full"
       />
     </div>
   );
