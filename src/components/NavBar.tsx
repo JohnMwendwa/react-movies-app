df5f28d44f82f870e754f4c891c337d7f875bc75
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-01-30 20:52:46 +0300

    import and render the SearchBar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,9 @@
+import SearchBar from "./SearchBar";
+
 export default function NavBar() {
   return (
     <header className="border-b ">
-      <div className="flex justify-between px-3 py-2">
+      <div className="flex justify-between items-center px-3 py-2">
         <a
           href="/"
           className="text-4xl font-medium border-2 border-green-400 px-2 pb-1 rounded-md"
@@ -10,6 +12,7 @@ export default function NavBar() {
           <span className="font-extrabold text-orange-400">_</span>
           <span className="font-extrabold text-green-400">O</span>M
         </a>
+        <SearchBar />
       </div>
     </header>
   );
