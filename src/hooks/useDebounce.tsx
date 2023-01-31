3378bb997f34de6baad32b8b07dd63c3fab9ec7c
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-01-31 15:00:29 +0300

    create useDebounce custom hook

---
+++ src/hooks/useDebounce.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+export default function useDebounce(value: string, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    let mount = true;
+    let timeout: number;
+
+    if (mount) {
+      timeout = setTimeout(() => {
+        setDebouncedValue(value);
+      }, delay);
+    }
+
+    return () => {
+      mount = false;
+      clearTimeout(timeout);
+    };
+  }, [value, delay]); // Only re-call effect if value or delay changes
+  return debouncedValue;
+}
