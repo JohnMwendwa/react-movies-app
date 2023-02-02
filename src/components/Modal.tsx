b3611319f2333da01219da9cbdbcbbb7b39a8a9c
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-02-03 00:26:21 +0300

    style the Modal component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,13 +1,24 @@
+import { ReactNode } from "react";
 import ReactDOM from "react-dom";
 
 interface ModalProps {
   openModal: boolean;
+  closeModal: () => void;
+  children: ReactNode;
 }
 
-export default function Modal({ openModal }: ModalProps) {
+export default function Modal({ openModal, closeModal, children }: ModalProps) {
   if (!openModal) return null;
   return ReactDOM.createPortal(
-    <div>Modal</div>,
+    <>
+      <div
+        className="fixed inset-0 z-[999px] bg-gray-600 opacity-75"
+        onClick={closeModal}
+      />
+      <div className="fixed top-[50%] left-[50%] z-[999px] -translate-x-1/2 -translate-y-1/2 bg-white p-8">
+        {children}
+      </div>
+    </>,
     document.getElementById("modal")!
   );
 }
