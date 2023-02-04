24388dcaa2bd39bd4058691128279eee8aa39fac
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-02-04 21:36:23 +0300

    add the pagination logic

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,3 +1,47 @@
+import { useSearchMoviesContext } from "../contexts/searchMoviesContext";
+import { usePagination, DOTS } from "../hooks/usePagination";
+
 export default function Pagination() {
-  return <div>Pagination</div>;
+  const { currentPage, limit, totalCount, onPageChange } =
+    useSearchMoviesContext();
+  const paginationRange = usePagination({ totalCount, limit, currentPage })!;
+
+  if (paginationRange?.length < 2) {
+    return null;
+  }
+  const onNext = () => {
+    onPageChange(currentPage + 1);
+  };
+  const onPrevious = () => {
+    onPageChange(currentPage - 1);
+  };
+  let lastPage = paginationRange[paginationRange?.length - 1];
+
+  return (
+    <nav className="text-center my-6 ">
+      <ul className="flex justify-center">
+        {currentPage > 1 && (
+          <li onClick={onPrevious} className=" cursor-pointer">
+            &lt;&lt; Prev
+          </li>
+        )}
+
+        {paginationRange.map((pageNumber) => {
+          if (pageNumber === DOTS) {
+            return <li>&#8230;</li>;
+          } else {
+            return (
+              <li onClick={() => onPageChange(pageNumber)}>{pageNumber}</li>
+            );
+          }
+        })}
+
+        {currentPage !== lastPage && (
+          <li onClick={onNext} className="cursor-pointer">
+            Next &gt;&gt;
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
 }
