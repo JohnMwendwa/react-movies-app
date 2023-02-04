a955eedc81ebfe33eb676b3a5261fa58720285f8
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-02-04 12:52:51 +0300

    allow the container to take the full width

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -6,7 +6,7 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <main className="max-w-5xl px-4 py-3 lg:px-6 lg:py-4 mx-auto">
+    <main className="max-w-5xl px-4 py-3 lg:px-6 lg:py-4 w-full mx-auto">
       {children}
     </main>
   );
