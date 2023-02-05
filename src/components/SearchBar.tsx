320fcb778f1df46130daada7c3421fefcd9cfade
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-02-05 16:06:20 +0300

    reset the current page state when user starts searching for movies

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,13 +1,19 @@
 import { useSearchMoviesContext } from "../contexts/searchMoviesContext";
 
 function SearchBar() {
-  const { query, setQuery } = useSearchMoviesContext();
+  const { query, setQuery, onPageChange } = useSearchMoviesContext();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+    onPageChange(1);
+  };
+
   return (
     <div>
       <input
         type="text"
         value={query}
-        onChange={(e) => setQuery(e.target.value)}
+        onChange={handleChange}
         placeholder="Quick Search Movies..."
         className="border border-green-400 outline-green-400 px-4 py-1 rounded-full"
       />
