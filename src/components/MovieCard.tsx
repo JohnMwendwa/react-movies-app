a15afa8709463a59226b03d94d643172fc6a8dd6
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-02-08 19:30:28 +0300

    lazy load the card images

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -13,6 +13,7 @@ export default function MovieCard(props: Movie) {
             alt={props.slug}
             width={210}
             height={315}
+            loading="lazy"
             className="rounded-md object-cover w-[210px] h-[auto] bg-gray-500 border-2 border-black "
           />
 
