3cf1d27fcd955f76a8aeb5dc7a2e473ac0134634
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-02-10 19:32:19 +0300

    remove the search bar fro the Home page to a shared navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,12 @@
 import MovieCard from "../components/MovieCard";
 import Pagination from "../components/Pagination";
 import { useSearchMoviesContext } from "../contexts/searchMoviesContext";
-import SearchBar from "../components/SearchBar";
 
-interface HomeProps {
-  openSearch: boolean;
-}
-
-export default function Home({ openSearch }: HomeProps) {
+export default function Home() {
   const { movies } = useSearchMoviesContext();
 
   return (
     <>
-      <div
-        className={`flex justify-center  py-4 absolute z-10 top-16 bg-gray-50 border-y left-0 right-0 ${
-          openSearch ? "block" : "hidden"
-        } md:hidden`}
-      >
-        <SearchBar />
-      </div>
       <h1 className="font-bold text-3xl text-center my-2 hidden md:block">
         Download YTS YIFY movies: HD smallest size
       </h1>
