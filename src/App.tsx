1358bd82288722a11c36110a0a1f884bdadc178a
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-05-20 11:00:45 +0300

    export the base url

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import NotFoundPage from "./pages/404";
 import Home from "./pages/Home";
 const MovieDetailsPage = lazy(() => import("./pages/MovieDetailsPage"));
 
-const BASE_URL = "/react-movies-app/";
+export const BASE_URL = "/react-movies-app/";
 
 function App() {
   const [openSearch, setOpenSearch] = useState(false);
