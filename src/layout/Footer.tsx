810f8ce56d0f64c9c8ba7fde720f7fb92e23f6b0
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-10-10 10:49:34 +0300

    add a border on top

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="mt-auto bg-gray-200 px-4 py-6">
+    <footer className="mt-auto border-t px-4 py-6">
       <p className="text-center">
         &copy; John Mwendwa {new Date().getFullYear()}
       </p>
