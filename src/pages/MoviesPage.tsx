99d20413785a1722d7098a52871124cdf17bc561
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2024-04-05 18:46:14 +0300

    ensure page id is valid before fetching movies

--- src/pages/MoviesPage.tsx
+++ src/pages/MoviesPage.tsx
@@ -3,16 +3,22 @@ import { useParams } from "react-router-dom";
 
 import { useSearchMoviesContext } from "../contexts/searchMoviesContext";
 import Home from "./Home";
+import NotFoundPage from "./404";
 
 const MoviesPage = () => {
-  const { onPageChange, currentPage } = useSearchMoviesContext();
+  const { onPageChange } = useSearchMoviesContext();
   const { pageId } = useParams();
 
   useEffect(() => {
-    if (pageId) {
-      onPageChange(parseInt(pageId));
+    if (pageId && !Number.isNaN(Number(pageId))) {
+      onPageChange(Number(pageId));
     }
   }, [pageId]);
+
+  if (Number.isNaN(Number(pageId))) {
+    return <NotFoundPage />;
+  }
+
   return <Home />;
 };
 
