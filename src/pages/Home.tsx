4dd403ed984195ec0fb418ba13aa4bbb26b45861
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2024-04-05 18:44:44 +0300

    Remove unnecesary useffect

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,13 @@
-import { useEffect } from "react";
-
 import MovieCard from "../components/MovieCard";
 import Pagination from "../components/Pagination";
 import { useSearchMoviesContext } from "../contexts/searchMoviesContext";
 
 export default function Home() {
-  const { movies, onPageChange } = useSearchMoviesContext();
-
-  useEffect(() => {
-    onPageChange(1);
-  }, []);
+  const { movies } = useSearchMoviesContext();
 
   return (
     <>
-      <h1 className="font-bold text-3xl text-center my-2 hidden md:block">
-        Download YTS YIFY movies: HD smallest size
-      </h1>
-      <div className="grid gap-4 grid-cols-[repeat(auto-fit,_210px)] place-content-center">
+      <div className="grid gap-8 grid-cols-[repeat(auto-fit,_240px)] place-content-center ">
         {movies.map((movie) => (
           <MovieCard key={movie.id} {...movie} />
         ))}
