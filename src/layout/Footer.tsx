c773988f08e13a55f3065c016f8e257e42b2304b
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2024-04-05 11:03:48 +0300

    Add github link to footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,8 +1,10 @@
 export default function Footer() {
   return (
-    <footer className="mt-auto border-t px-4 py-6">
+    <footer className="mt-auto border-t border-white/10  px-4 py-8">
       <p className="text-center">
-        &copy; John Mwendwa {new Date().getFullYear()}
+        &copy;
+        <a href="https://github.com/JohnMwendwa"> John Mwendwa </a>
+        {new Date().getFullYear()}
       </p>
     </footer>
   );
