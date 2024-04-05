d955e2f6389919f9e48c5376a535ffc159aeeb57
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2024-04-05 10:54:49 +0300

    Move navbar and footer component to layouts component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { lazy, Suspense, useState } from "react";
+import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 
-import NavBar from "./components/NavBar";
 import { SearchMoviesProvider } from "./contexts/searchMoviesContext";
-import Footer from "./layout/Footer";
 import Layout from "./layout/Layout";
 import NotFoundPage from "./pages/404";
 import Home from "./pages/Home";
@@ -14,30 +12,25 @@ const MovieDetailsPage = lazy(() => import("./pages/MovieDetailsPage"));
 export const BASE_URL = "/";
 
 function App() {
-  const [openSearch, setOpenSearch] = useState(false);
   return (
-    <>
-      <SearchMoviesProvider>
-        <NavBar openSearch={openSearch} setOpenSearch={setOpenSearch} />
-        <Layout>
-          <Routes>
-            <Route path={BASE_URL} element={<Home />} />
-            <Route
-              path={`${BASE_URL}movies/:id`}
-              element={
-                <Suspense fallback="Loading...">
-                  <MovieDetailsPage />
-                </Suspense>
-              }
-            />
-            <Route path={`${BASE_URL}page/:pageId`} element={<MoviesPage />} />
+    <SearchMoviesProvider>
+      <Layout>
+        <Routes>
+          <Route path={BASE_URL} element={<Home />} />
+          <Route
+            path={`${BASE_URL}movies/:id`}
+            element={
+              <Suspense fallback="Loading...">
+                <MovieDetailsPage />
+              </Suspense>
+            }
+          />
+          <Route path={`${BASE_URL}page/:pageId`} element={<MoviesPage />} />
 
-            <Route path="/*" element={<NotFoundPage />} />
-          </Routes>
-        </Layout>
-      </SearchMoviesProvider>
-      <Footer />
-    </>
+          <Route path="/*" element={<NotFoundPage />} />
+        </Routes>
+      </Layout>
+    </SearchMoviesProvider>
   );
 }
 
