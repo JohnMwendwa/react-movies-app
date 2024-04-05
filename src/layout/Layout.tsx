3364284d0d7c794f283bce94cf80d35d510a900b
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2024-04-05 11:33:59 +0300

    Add Navbar and footer component and change background color

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,13 +1,20 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
+import NavBar from "../components/NavBar";
+import Footer from "./Footer";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
+  const [openSearch, setOpenSearch] = useState(false);
   return (
-    <main className="max-w-5xl px-4 py-3 lg:px-6 lg:py-4 w-full mx-auto">
-      {children}
-    </main>
+    <div className="flex flex-col min-h-screen bg-[#08081b] text-orange-600">
+      <NavBar openSearch={openSearch} setOpenSearch={setOpenSearch} />
+      <main className="max-w-7xl px-4 py-8 sm:py-16 w-full mx-auto">
+        {children}
+      </main>
+      <Footer />
+    </div>
   );
 }
