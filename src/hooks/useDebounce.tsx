5abda22bf73b1ceb48303da5c48373da42502b9e
AeolusZane <aeoluj@gmail.com>
2024-09-23 10:57:23 +0800

    useDebounce is a bit weird.
    useDebounce is a bit weird. u may mean useThrottle? btw, variable mount is useless there~

--- src/hooks/useDebounce.tsx
+++ src/hooks/useDebounce.tsx
@@ -4,17 +4,11 @@ export function useDebounce(value: string, delay: number) {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
-    let mount = true;
-    let timeout: number;
-
-    if (mount) {
-      timeout = setTimeout(() => {
+    const timeout = setTimeout(() => {
         setDebouncedValue(value);
       }, delay);
-    }
 
     return () => {
-      mount = false;
       clearTimeout(timeout);
     };
   }, [value, delay]); // Only re-call effect if value or delay changes
